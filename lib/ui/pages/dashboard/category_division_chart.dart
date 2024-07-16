c2580da9c04f9ab13a0f3efa19186eb1378ad042
pixelsapphire <35204889+pixelsapphire@users.noreply.github.com>
2024-07-16 13:44:16 +0200

    Add percentage to category division charts

--- lib/ui/pages/dashboard/category_division_chart.dart
+++ lib/ui/pages/dashboard/category_division_chart.dart
@@ -1,6 +1,8 @@
 import 'package:aurum/data/objects/category.dart';
+import 'package:aurum/ui/theme.dart';
 import 'package:aurum/ui/widgets/money_label.dart';
 import 'package:aurum/util/extensions.dart';
+import 'package:aurum/util/utility.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
@@ -19,51 +21,56 @@ class _CategoryDivisionChartState extends State<CategoryDivisionChart> {
 
   @override
   Widget build(BuildContext context) => widget.data.entries.toList().op(
-        (entries) => Stack(
-          children: [
-            SfCircularChart(
-              palette: entries
-                  .map((e) => e.key.color.withValue(_selectedCategory != null && e.key != _selectedCategory ? 0.5 : 1))
-                  .nullIfEmpty()
-                  ?.toList(),
-              series: [
-                DoughnutSeries<MapEntry<Category, double>, String>(
-                  dataSource: entries,
-                  xValueMapper: (entry, _) => entry.key.name,
-                  yValueMapper: (entry, _) => entry.value,
-                  radius: '100%',
-                  innerRadius: '70%',
-                  animationDuration: 0,
-                  onPointTap: (slice) => setState(() {
-                    final tappedCategory = slice.pointIndex != null ? entries[slice.pointIndex!].key : null;
-                    _selectedCategory = _selectedCategory == tappedCategory ? null : tappedCategory;
-                  }),
-                ),
-              ],
-            ),
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    _selectedCategory?.name ?? widget.totalLabel,
-                    style: TextStyle(
-                      color: CupertinoDynamicColor.resolve(CupertinoColors.secondaryLabel, context),
-                      fontSize: 20,
-                    ),
-                  ),
-                  MoneyLabel(
-                    _selectedCategory != null
-                        ? widget.data[_selectedCategory]?.abs() ?? 0
-                        : widget.data.values.nullIfEmpty()?.reduce((a, b) => a + b).abs() ?? 0,
-                    suffix: ' PLN',
-                    style: const TextStyle(fontSize: 30),
+        (entries) {
+          final double total = sum(entries.map((e) => e.value));
+          return Stack(
+            children: [
+              SfCircularChart(
+                palette: entries
+                    .map((e) => e.key.color.withValue(_selectedCategory != null && e.key != _selectedCategory ? 0.5 : 1))
+                    .nullIfEmpty()
+                    ?.toList(),
+                series: [
+                  DoughnutSeries<MapEntry<Category, double>, String>(
+                    dataSource: entries,
+                    xValueMapper: (entry, _) => entry.key.name,
+                    yValueMapper: (entry, _) => entry.value,
+                    radius: '100%',
+                    innerRadius: '70%',
+                    animationDuration: 0,
+                    onPointTap: (slice) => setState(() {
+                      final tappedCategory = slice.pointIndex != null ? entries[slice.pointIndex!].key : null;
+                      _selectedCategory = _selectedCategory == tappedCategory ? null : tappedCategory;
+                    }),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 16),
+                    Text(
+                      _selectedCategory?.name ?? widget.totalLabel,
+                      style: TextStyle(color: AurumColors.foregroundSecondary(context), fontSize: 20),
+                    ),
+                    MoneyLabel(
+                      _selectedCategory != null
+                          ? widget.data[_selectedCategory]?.abs() ?? 0
+                          : widget.data.values.nullIfEmpty()?.reduce((a, b) => a + b).abs() ?? 0,
+                      suffix: ' PLN',
+                      style: const TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      _selectedCategory != null ? ((widget.data[_selectedCategory] ?? 0) / total).asPercent(1) : '',
+                      style: TextStyle(color: AurumColors.foregroundPrimary(context), fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
       );
 }
