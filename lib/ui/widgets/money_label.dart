a20385c0429dcb68dd009191547b3cf0b91de88f
pixelsapphire <35204889+pixelsapphire@users.noreply.github.com>
2024-07-24 17:31:42 +0200

    Fix the negative zero issue in MoneyLabel

--- lib/ui/widgets/money_label.dart
+++ lib/ui/widgets/money_label.dart
@@ -1,4 +1,5 @@
 import 'package:aurum/ui/theme.dart';
+import 'package:aurum/util/extensions.dart';
 import 'package:flutter/cupertino.dart';
 
 class MoneyLabel extends StatelessWidget {
@@ -9,17 +10,15 @@ class MoneyLabel extends StatelessWidget {
   const MoneyLabel(this.amount, {super.key, this.style, this.prefix, this.suffix});
 
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        if (prefix != null) Text(prefix!, style: TextStyle(color: AurumColors.foregroundPrimary(context)).appendedTo(style)),
-        Text(
-          amount.toStringAsFixed(2),
-          style: TextStyle(fontWeight: FontWeight.bold, color: AurumColors.foregroundPrimary(context)).appendedTo(style),
-        ),
-        if (suffix != null) Text(suffix!, style: TextStyle(color: AurumColors.foregroundPrimary(context)).appendedTo(style)),
-      ],
-    );
-  }
+  Widget build(BuildContext context) => Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (prefix != null) Text(prefix!, style: TextStyle(color: AurumColors.foregroundPrimary(context)).appendedTo(style)),
+          Text(
+            (amount.roundToPlaces(2) != 0 ? amount : 0).toStringAsFixed(2), // to avoid negative zero
+            style: TextStyle(fontWeight: FontWeight.bold, color: AurumColors.foregroundPrimary(context)).appendedTo(style),
+          ),
+          if (suffix != null) Text(suffix!, style: TextStyle(color: AurumColors.foregroundPrimary(context)).appendedTo(style)),
+        ],
+      );
 }
