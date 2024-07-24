bc8770cb91e05c53f702cdf9a5bdaa79e03fec86
pixelsapphire <35204889+pixelsapphire@users.noreply.github.com>
2024-07-25 00:44:36 +0200

    Add a warning message to the database console

--- lib/ui/pages/more/console.dart
+++ lib/ui/pages/more/console.dart
@@ -1,6 +1,7 @@
 import 'package:aurum/data/database.dart';
 import 'package:aurum/ui/pages/page_base.dart';
 import 'package:aurum/ui/theme.dart';
+import 'package:aurum/ui/widgets/placeholder.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -43,79 +44,116 @@ class _ConsoleState extends State<Console> {
     return false;
   }
 
+  Widget _buildWarning(BuildContext context) => LayoutBuilder(
+      builder: (context, constraints) => EmptyListPlaceholder(
+            icon: Icons.warning_amber_outlined,
+            title: 'Warning',
+            message: SizedBox(
+              width: constraints.maxWidth,
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 4),
+                      child: Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'The database console is a powerful tool meant primarily for debugging purposes. '
+                                  'You should only proceed if you know what you are doing. Otherwise ',
+                            ),
+                            TextSpan(
+                              text: 'you may accidentally damage or delete your data permanently',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: '.')
+                          ],
+                        ),
+                      ),
+                    ),
+                    Text('Enter \'help\' to get started.'),
+                  ],
+                ),
+              ),
+            ),
+          ));
+
+  Widget _buildLog(BuildContext context) => ListView.builder(
+        itemCount: _log.length,
+        itemBuilder: (context, index) {
+          final (type, text) = _log[index];
+          final Color color = type == EntryType.error ? CupertinoColors.systemRed : AurumColors.foregroundPrimary(context);
+          if (type == EntryType.command) {
+            return Row(
+              children: [
+                Icon(CupertinoIcons.right_chevron, size: 24, color: color),
+                Expanded(
+                  child: Container(
+                    color: AurumColors.backgroundTertiary(context),
+                    child: Padding(padding: const EdgeInsets.all(2), child: SelectableText(text)),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: DecoratedBox(
+                decoration: BoxDecoration(border: Border(left: BorderSide(color: color, width: 4))),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 14),
+                  child: SelectableText(text, style: TextStyle(color: color)),
+                ),
+              ),
+            );
+          }
+        },
+      );
+
+  Widget _buildInput(BuildContext context) => Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: Icon(CupertinoIcons.right_chevron, size: 20, color: AurumColors.foregroundSecondary(context)),
+          ),
+          Expanded(
+            child: CupertinoTextField(
+              controller: _inputController,
+              placeholder: 'Enter command',
+              style: const TextStyle(fontSize: 24),
+              padding: const EdgeInsets.all(8),
+            ),
+          ),
+          CupertinoButton(
+            child: const Text('Execute'),
+            onPressed: () {
+              setState(() {
+                if (_inputController.text.isEmpty) return;
+                _log.add((EntryType.command, _inputController.text));
+                final bool handled = _execute(_inputController.text);
+                if (!handled) {
+                  AurumDatabase.executeRaw(_inputController.text).then(
+                    (response) => setState(() => _log.add((EntryType.response, response))),
+                    onError: (error) => setState(() => _log.add((EntryType.error, error.toString()))),
+                  );
+                }
+                _inputController.clear();
+              });
+            },
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) => PageBase(
         builtInScrollView: false,
         navigationBar: const CupertinoNavigationBar(middle: Text('Database console')),
         child: Column(
           children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: _log.length,
-                itemBuilder: (context, index) {
-                  final (type, text) = _log[index];
-                  final Color color =
-                      type == EntryType.error ? CupertinoColors.systemRed : AurumColors.foregroundPrimary(context);
-                  if (type == EntryType.command) {
-                    return Row(
-                      children: [
-                        Icon(CupertinoIcons.right_chevron, size: 24, color: color),
-                        Expanded(
-                          child: Container(
-                            color: AurumColors.backgroundTertiary(context),
-                            child: Padding(padding: const EdgeInsets.all(2), child: SelectableText(text)),
-                          ),
-                        ),
-                      ],
-                    );
-                  } else {
-                    return Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(border: Border(left: BorderSide(color: color, width: 4))),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 14),
-                          child: SelectableText(text, style: TextStyle(color: color)),
-                        ),
-                      ),
-                    );
-                  }
-                },
-              ),
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 4),
-                  child: Icon(CupertinoIcons.right_chevron, size: 20, color: AurumColors.foregroundPrimary(context)),
-                ),
-                Expanded(
-                  child: CupertinoTextField(
-                    controller: _inputController,
-                    placeholder: 'Enter command',
-                    style: const TextStyle(fontSize: 24),
-                    padding: const EdgeInsets.all(8),
-                  ),
-                ),
-                CupertinoButton(
-                  child: const Text('Execute'),
-                  onPressed: () {
-                    setState(() {
-                      if (_inputController.text.isEmpty) return;
-                      _log.add((EntryType.command, _inputController.text));
-                      final bool handled = _execute(_inputController.text);
-                      if (!handled) {
-                        AurumDatabase.executeRaw(_inputController.text).then(
-                          (response) => setState(() => _log.add((EntryType.response, response))),
-                          onError: (error) => setState(() => _log.add((EntryType.error, error.toString()))),
-                        );
-                      }
-                      _inputController.clear();
-                    });
-                  },
-                ),
-              ],
-            ),
+            Expanded(child: _log.isNotEmpty ? _buildLog(context) : _buildWarning(context)),
+            _buildInput(context),
           ],
         ),
       );
